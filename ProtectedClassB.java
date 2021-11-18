ebce917bb3e5ded7863edbf009e17020f6f17dcc
aarshgarg <79684936+aarshgarg@users.noreply.github.com>
2021-11-18 23:21:40 +0530

    Add files via upload
    protected class in which we can inherit the one [ackage in diffrent package

---
+++ ProtectedClassB.java
@@ -0,0 +1,12 @@
+package practice;
+
+import pack.*;
+
+public class ProtectedClassB extends ProtectedClassA {
+    public static void main(String[] args)
+
+    {
+    ProtectedClassB obj1 = new ProtectedClassB();
+    obj1.get();
+}
+}
