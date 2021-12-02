d4b0449ddae225fbdddca05586a8154c594ebd05
Aarsh <79684936+aarshgarg@users.noreply.github.com>
2021-12-03 01:42:35 +0530

    Add files via upload
    in this we use the thread package and in main after the instance of the thread class then we can define the instance of the class  with start

---
+++ MultiThreading.java
@@ -0,0 +1,35 @@
+package practice;
+
+class MyThread1 extends Thread {
+ //  @Override
+    public void run() {
+        int i=0;
+        while (i<20) {
+            System.out.println("Thread is run concurrentlyy");
+            System.out.println("It is first thread");
+            i++;
+        }
+    }
+}
+        class MyThread2 extends Thread {
+  //  @Override
+            public void run() {
+                int i=0;
+                while (i<20) {
+                    System.out.println("Means That it give some thing  ");
+                    System.out.println("It s  happy");
+                    i++;
+                }
+            }
+        }
+
+
+public class MultiThreading {
+    public static void main(String[] args) {
+        MyThread1 t1=new MyThread1();
+        MyThread2 t2= new MyThread2();
+        t1.start();
+        t2.start();
+    }
+}
+
